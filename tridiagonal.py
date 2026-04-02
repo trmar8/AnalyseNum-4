e4c5752c2e93686d23ffb3d68e1bdf4684f657cf
seb20100 <sevin7@ulaval.ca>
2026-04-02 16:30:08 -0400

    v1 (#1)

--- tridiagonal.py
+++ tridiagonal.py
@@ -2,10 +2,10 @@ import numpy as np
 
 def tridiagonal(N, D, I, S, b):
     # Création de copies pour ne pas modifier les vecteurs originaux
-    #D = np.array(D, dtype=float).copy()
-    #I = np.array(I, dtype=float)
-    #S = np.array(S, dtype=float).copy()
-    #b = np.array(b, dtype=float)
+    D = np.array(D, dtype=float).copy()
+    I = np.array(I, dtype=float)
+    S = np.array(S, dtype=float).copy()
+    b = np.array(b, dtype=float)
 
     y = np.zeros(N)
     x = np.zeros(N)
